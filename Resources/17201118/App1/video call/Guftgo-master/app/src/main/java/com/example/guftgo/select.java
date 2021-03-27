d72fd62a59a5e96fdf686610f053c3c612a706c2
Iqbal Hasan Khan <17201118@uap-bd.edu>
2021-03-28 01:17:01 +0600

    newcode

---
+++ Resources/17201118/App1/video call/Guftgo-master/app/src/main/java/com/example/guftgo/select.java
@@ -0,0 +1,14 @@
+package com.example.guftgo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class select extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select);
+    }
+}
